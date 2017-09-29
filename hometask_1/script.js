1b646611e814e0f405d46bbfa1b8fae41fdae5f7
DaryaBeloded <dariabeloded@gmail.com>
2017-09-29 21:29:37 +0300

    add first home tasks

--- hometask_1/script.js
+++ hometask_1/script.js
@@ -4,57 +4,57 @@
 //3. Написать трехшаговый калькуллятор. Алгоритм: 3 строки: число 1, операция, второе число
 
 //Task №1
-// var str = prompt("Enter a string");
-// console.log(str);
-// var k = 0;
+var str = prompt("Enter a string");
+console.log(str);
+var k = 0;
 
-// for(var i = 0; i < str.length; i++)
-// {
-// 	if(str[i] == "(")
-// 		k++;
-// 	else 
-// 		if(str[i] == ")")
-// 		k--;
+for(var i = 0; i < str.length; i++)
+{
+	if(str[i] == "(")
+		k++;
+	else 
+		if(str[i] == ")")
+		k--;
 
-// 	//console.log(k);
+	//console.log(k);
 
-// 	if(k < 0)
-// 		break;
-// }
+	if(k < 0)
+		break;
+}
 
-// if(k == 0)
-// 	console.log("true");
-// else
-// 	console.log("false");
-// //--------------------------------------------------------
+if(k == 0)
+	console.log("true");
+else
+	console.log("false");
+//--------------------------------------------------------
 
-// //Task №3
-// var a = Number(prompt("Enter the first value: "));
-// var sign = prompt("Enter the operation: ");
-// var b = Number(prompt("Enter the second value: "));
+//Task №3
+var a = Number(prompt("Enter the first value: "));
+var sign = prompt("Enter the operation: ");
+var b = Number(prompt("Enter the second value: "));
 
-// if(a && b || a == 0 || b == 0)
-// 	switch(sign) {
-// 		case '+': 
-// 			console.log("The sum of " + a + " and " + b + " iquals " + (a + b));
-// 			break;
-// 		case '-':
-// 			console.log("The difference of " + a + " and " + b + " iquals " + (a - b));
-// 			break;
-// 		case '*':
-// 			console.log("The product of " + a + " and " + b + " iquals " + (a * b));
-// 			break;
-// 		case '/':
-// 			console.log("The result of the division " + a + " and " + b + " iquals " + (a / b);
-// 			break;
-// 		case '^':
-// 			console.log(Math.pow(a, b));
-// 			break;
-// 		default:
-// 			console.log("It is not an operation");
-// 	}
-// else
-// 	console.log("It is not a numbers");
+if(a && b || a == 0 || b == 0)
+	switch(sign) {
+		case '+': 
+			console.log("The sum of " + a + " and " + b + " iquals " + (a + b));
+			break;
+		case '-':
+			console.log("The difference of " + a + " and " + b + " iquals " + (a - b));
+			break;
+		case '*':
+			console.log("The product of " + a + " and " + b + " iquals " + (a * b));
+			break;
+		case '/':
+			console.log("The result of the division " + a + " and " + b + " iquals " + (a / b);
+			break;
+		case '^':
+			console.log(Math.pow(a, b));
+			break;
+		default:
+			console.log("It is not an operation");
+	}
+else
+	console.log("It is not a numbers");
 //--------------------------------------------------------
 
 //Task №2
