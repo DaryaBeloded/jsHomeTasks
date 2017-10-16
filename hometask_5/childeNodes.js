b2e3eb6dbdc239757771c8aac8da57c93e83a175
DaryaBeloded <dariabeloded@gmail.com>
2017-10-16 09:28:49 +0300

    add Erythrophene sieve, herd instinct and clock and childNodes

---
+++ hometask_5/childeNodes.js
@@ -0,0 +1,17 @@
+var numberoftag = document.getElementsByTagName("*").length; 
+
+var numberofcomment = 0, numberoftext = 0; 
+var childNodes = document.childNodes; 
+
+var rec = function(obj){ 
+if (obj === undefined) return false; 
+for (var i = 0; i < obj.length; i++){ 
+rec(obj[i].childNodes); 
+if (obj[i].nodeType == 3) numberoftext++; 
+if (obj[i].nodeType == 8) numberofcomment++; 
+} 
+} 
+rec(childNodes); 
+alert("Number of tag: "+numberoftag); 
+alert("Number of text: "+numberoftext); 
+alert("Number of comment: "+numberofcomment);
