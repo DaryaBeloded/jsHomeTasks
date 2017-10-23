c4c10ad0b49e6e0714a93c91ea38bffcbb901bf6
DaryaBeloded <dariabeloded@gmail.com>
2017-10-23 10:57:49 +0300

    some changes in Erythrophene sieve and validation

--- hometask_5/script.js
+++ hometask_5/script.js
@@ -1,123 +1,135 @@
 var table = document.getElementsByTagName("table")[0];
-var arr = [];
+// var arr = [];
 var size = prompt("Enter a size of array: ");
-for(var i = 2; i <= size; i++){
-	arr[i] = i;
-}
-console.log(arr.length);
+// for(var i = 2; i <= size; i++){
+// 	arr[i] = i;
+// }
+// console.log(arr.length);
+var TableCells = document.getElementsByTagName("td");
 
-for(var i = 1; i < arr.length; i++){
-	if((i-1)%10==0){
+var tableSize = Math.round(Math.sqrt(size));
+for(var i = 1; i <= size; i++){
+	if((i-1)%tableSize==0){
 		var tr = document.createElement("tr");
 		table.appendChild(tr);
 	}
 	if(i==1){
 		var td = document.createElement("td");
 		td.setAttribute("id", "first");
-		table.appendChild(td);
+		table.appendChild(td);	
 	}	
 	else{
 		var td = document.createElement("td");
-		td.setAttribute("id", "i"+i);
 		table.appendChild(td);
-		td.innerText = arr[i];
+		td.innerText = i;
 	}
 }
 
-var p = arr[2];
-var time = 0;
-do{
-	for( i = 2*p; i< arr.length; i+= p){
-		setTimeout((function(i){
-			return function(){
-				var str = "i" + i;
-				var td = document.getElementById(str);
-				td.setAttribute("a","notSimple");
-				}
-		})(i), time)
-		time+=250;
+function getTableCellWithNumber(n){
+	return TableCells[n-1];
+}
+
+// Current P
+var currentN = 2;
+// Last number that was marked as not simple
+var lastMarked = 2;
+
+// Array with all numbers that was marked
+var marked = [];
+
+var N = size;
+
+window.mainInterval = window.setInterval(function(){
+	lastMarked += currentN;
+	if(lastMarked > N){
+		do{
+			currentN++;
+			lastMarked = currentN*2;
+		}while(marked.indexOf(currentN)!=-1)
 	}
-	for( i=p+1; i<arr.length; i++){
-		var str = "i" + i;
-		var td = document.getElementById(str);
-		if(!td.hasAttribute("a")){
-			break;
+	
+	
+	
+	
+	//console.log(currentN > N)
+	if(currentN > N){
+		return window.clearInterval(window.mainInterval);
+	}
+	printSimpleCell(currentN);
+	
+	if(currentN > Math.sqrt(N)+1){
+		return;	
+		// Print all tds with no class notSimple
+		//printPrimes();
+	}
+
+	getTableCellWithNumber(lastMarked).setAttribute("class","notSimple");
+	if(marked.indexOf(lastMarked)==-1){
+		marked.push(lastMarked);
+	}
+},25);
+
+function printSimpleCell(n){
+	getTableCellWithNumber(n).setAttribute("class", "simple");
+}
+
+function printPrimes(){
+	var TableCellsWithPrimes = document.querySelectorAll('td:not([class="notSimple"])');
+	var i = 0;
+	var interval = window.setInterval(function(){
+		TableCellsWithPrimes[i].setAttribute("class","simple");
+		if(++i >= TableCellsWithPrimes.length){
+			window.clearInterval(interval);
 		}
-	}
-	p = i;
-} while(p*p<arr.length-1);
-
-var arr2=[];
-for(var i = 2, k=0; i < arr.length; i++){
-	var str = "i" + i;
-	var td = document.getElementById(str);
-	if(!td.hasAttribute("a")){
-		arr2[k] = arr[i];
-		k++;
-	}
-}
-
-for(var i = 0; i<arr2.length; i++){
-	for(var l = 2; l<arr.length; l++){
-		if(arr2[i] == arr[l]){
-				var str = "i" + arr2[i];
-				var td = document.getElementById(str);
-				td.className = "simple";
-			
-		}
-	}
+	},200)
 }
 
 
-// for (var i = 2; i <= size; i++) {
-//   arr[i] = true
-// }
-
-// for(var i = 1; i < arr.length; i++){
-// 	if((i-1)%10==0){
-// 		var tr = document.createElement("tr");
-// 		table.appendChild(tr);
-// 	}
-// 	if(i==1){
-// 		var td = document.createElement("td");
-// 		td.setAttribute("id", "first");
-// 		table.appendChild(td);
-// 	}	
-// 	else{
-// 		var td = document.createElement("td");
-// 		td.setAttribute("id", "i"+i);
-// 		table.appendChild(td);
-// 		td.innerText = i;
-// 	}
-// }
-
-// var p = 2;
-// do {
-//   for (i = 2 * p; i < size; i += p) {
-//     arr[i] = false;
-//   }
-//   for (i = p + 1; i < size; i++) {
-//     if (arr[i]) break;
-//   }
-//   p = i;
-// } while (p * p < size); 
-
+// var p = arr[2];
 // var time = 0;
-// for(var i=2; i<arr.length; i++){
-// 	setTimeout((function(n){
-// 		return function(){
-// 			if(!arr[i]){
-// 				console.log(i);
+// do{
+// 	for( i = 2*p; i< arr.length; i+= p){
+// 		setTimeout((function(i){
+// 			return function(){
 // 				var str = "i" + i;
-// 				console.log(str);
 // 				var td = document.getElementById(str);
-// 				// td.className = "notSimple";
-// 				td.style.backgroundColor = 'red';
-// 			}
+// 				td.setAttribute("a","notSimple");
+// 				}
+// 		})(i), time)
+// 		time+=250;
+// 	}
+// 	for( i=p+1; i<arr.length; i++){
+// 		var str = "i" + i;
+// 		var td = document.getElementById(str);
+// 		if(!td.hasAttribute("a")){
+// 			break;
 // 		}
-// 	})(i), time)
-// 	time += 250;
+// 	}
+// 	p = i;
+// } while(p*p<arr.length-1);
+
+// var arr2=[];
+// for(var i = 2, k=0; i < arr.length; i++){
+// 	var str = "i" + i;
+// 	var td = document.getElementById(str);
+// 	if(!td.hasAttribute("a")){
+// 		arr2[k] = arr[i];
+// 		k++;
+// 	}
 // }
+
+// for(var i = 0; i<arr2.length; i++){
+// 	for(var l = 2; l<arr.length; l++){
+// 		if(arr2[i] == arr[l]){
+// 				var str = "i" + arr2[i];
+// 				var td = document.getElementById(str);
+// 				td.className = "simple";
+			
+// 		}
+// 	}
+// }
+
+
 
 
 
