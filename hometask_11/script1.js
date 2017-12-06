601a68d2032aae3c2b1d1c9abc6153fe119832ce
Beloded Darya <dariabeloded@gmail.com>
2017-12-07 00:46:57 +0300

    UPD

--- hometask_11/script1.js
+++ hometask_11/script1.js
@@ -11,37 +11,55 @@ xhr.onload = function(){
 	var male = document.getElementById("male");
 	data.forEach(function(elem){
 		var content = document.createElement("div");
-		if(elem.title == "female"){
-			content.classList.add("female");
-		}
-		if(elem.title == "male"){
-			content.classList.add("male");
-		}
+
 		content.style.display="none";
 		content.style.position = "absolute";
-		elem.categories.forEach(function(item){
-			var div = document.createElement("div");
-			div.innerHTML = "<p>" + item.name + "</p><ul class = 'ul1'></ul>";
-			var ul1 = div.children[1];
-			item.items.forEach(function(elem1){
-				var li = document.createElement("li");
-				li.innerText = elem1.itemName;
-				ul1.appendChild(li);
-			})
-			content.appendChild(div);
-		}) 
+		
+		var xhr1 = new XMLHttpRequest();
+		xhr1.open("GET", elem.categories, true);
+		xhr1.onload = function(){
+			var data1 = JSON.parse(this.responseText);			
+			for(var i = 0; i < data1.length; i++){				
+				(function(n){
+					var xhr2 = new XMLHttpRequest();
+					xhr2.open("GET", data1[n].items, true);
+
+					var div = document.createElement("div");
+					div.innerHTML = "<p>" + data1[i].name + "</p><ul class = 'ul1'></ul>";
+					var ul1 = div.children[1];
+
+					xhr2.onload = function(){
+						var data2 = JSON.parse(this.responseText);
+						for(var k = 0; k < data2.length; k++){
+							var li = document.createElement("li");
+							li.innerText = data2[k].itemName;
+							ul1.appendChild(li);
+							(function(n){
+							li.addEventListener("click", function(e){
+								contentDiv1.innerHTML = data2[n].img;
+							})
+						})(k)
+						}
+					}
+					xhr2.send();
+					content.appendChild(div);
+				})(i)
+			}
+			if(elem.title == "female"){
+				content.classList.add("female");
+				document.getElementsByClassName("female")[0].children[0].style.float = "left";
+				document.getElementsByClassName("female")[0].children[1].style.float = "right";
+				document.getElementsByClassName("female")[0].children[1].style.marginLeft = "74px";
+			}
+			if(elem.title == "male"){
+				content.classList.add("male");
+				document.getElementsByClassName("male")[0].children[0].style.float = "left";
+				document.getElementsByClassName("male")[0].children[1].style.float = "right";
+				document.getElementsByClassName("male")[0].children[1].style.marginLeft = "74px";
+			}
+		}
+		xhr1.send();
 		contentDiv.appendChild(content);
-	})
-
-	//--------------STYLE----------------------------
-	document.getElementsByClassName("female")[0].children[0].style.float = "left";
-	document.getElementsByClassName("female")[0].children[1].style.float = "right";
-	document.getElementsByClassName("female")[0].children[1].style.marginLeft = "74px";
-
-	document.getElementsByClassName("male")[0].children[0].style.float = "left";
-	document.getElementsByClassName("male")[0].children[1].style.float = "right";
-	document.getElementsByClassName("male")[0].children[1].style.marginLeft = "74px";
-	//-----------------------------------------------
 
 	female.addEventListener("click", function(){
 		contentDiv1.innerHTML = "";
@@ -57,18 +75,6 @@ xhr.onload = function(){
 		}
 		document.getElementsByClassName("male")[0].style.display = "block";
 	})
-	contentDiv.addEventListener("click", function(e){
-		if(e.target.parentNode.getAttribute("class") == "ul1"){
-				for(var i = 0; i < data.length; i++){
-					for(var k = 0; k < data[i].categories.length; k++){
-						for(var k1 = 0; k1 < data[i].categories[k].items.length; k1++){
-							if(data[i].categories[k].items[k1].itemName == e.target.outerText){
-								contentDiv1.innerHTML = data[i].categories[k].items[k1].img;
-							}
-						}
-					}
-				}
-		}
-	})
+})
 }
 xhr.send();
